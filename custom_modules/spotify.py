b61746191f5e2c8fd5bcb8181bd150209b1a1856
Dom Neuner <dom@neuner.uk>
2020-05-17 14:19:55 +0200

    I'm not ignorant, I'm just ignoring useless shit

--- custom_modules/spotify.py
+++ custom_modules/spotify.py
@@ -11,7 +11,7 @@ client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
 redirect_uri = os.environ['SPOTIFY_CALLBACK_URL']
 scope = 'user-read-recently-played user-read-playback-state user-modify-playback-state user-read-currently-playing'
 
-def spotify_check():
+def check():
     token = util.prompt_for_user_token(username=username,
                                        scope=scope,
                                        client_id=client_id,
@@ -23,10 +23,11 @@ def spotify_check():
     spotify = spotipy.Spotify(auth=token)
 
     current_playback = spotify.current_playback('DE')
-    print(current_playback['device']['id'])
-    file_obj = open("output.txt","w+")
-    file_obj.write(current_playback)
+    current_title = current_playback['item']['name']
+    current_artist = current_playback['item']['artists'][0]['name']
+    print(f"The current song title is: {current_title}")
+    print(f"The current artist is: {current_artist}")
 
 
 if __name__ == '__main__':
-    spotify_check()
+    check()
